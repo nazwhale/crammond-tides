90fa5d5f21ebf9cc5b9cecd51e030dfbdb5f5aa0
Naz Malik <nazmalik08@gmail.com>
2021-03-16 13:18:57 +0000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  siteMetadata: {
+    title: "Crammond Tides",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "265834876",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+  ],
+};
